ba65d93fd8c67237a9b8dd30f01af266e7b85d78
yann <yann.boisselier@gmail.com>
2017-08-21 14:51:25 +0200

    fix : non / yes was clickable

--- components/WeatherHour.js
+++ components/WeatherHour.js
@@ -3,22 +3,26 @@ import { round } from 'lodash'
 import moment from 'moment'
 moment.locale('fr')
 
-const defaults = {
-  color: 'goldenrod',
-  size: 256,
-  animate: true
-}
-
-const Button = ({ precipProbability }) => {
-  const ButtonText = precipProbability > 0 ? 'oui' : 'non'
-  const ButtonClass = precipProbability > 0 ? 'button is-danger' : 'button is-success'
+const YesNo = ({ precipProbability }) => {
+  const YesNoText = precipProbability > 0 ? 'oui' : 'non'
+  const YesNoClass = precipProbability > 0 ? 'yesno is-danger' : 'yesno is-success'
   return (
     <div>
-      <a className={ButtonClass}>{ButtonText}</a>
+      <span className={YesNoClass}>{YesNoText}</span>
       <style jsx>{`
-      .button {
+      .yesno {
+        display:inline-block;
+        border-radius: 0.5rem;
+        color:white;
+        padding:2rem;
         font-size: 3rem;
         text-transform: uppercase
+      }
+      .yesno.is-success{
+        background: #23d160;
+      }
+      .yesno.is-danger{
+        background: #ff3860;
       }
     `}</style>
     </div>
@@ -37,7 +41,7 @@ const WeatherHour = ({ meteo }) => {
   return (
     <div className="content section">
       <h2> <ReadableTime unixtime={meteo.time} /> </h2>
-      <Button precipProbability={meteo.precipProbability} />
+      <YesNo precipProbability={meteo.precipProbability} />
       <h2>Probabilité de précipitation: {round(meteo.precipProbability * 100)}% </h2>
       <h2>Intensité de la précipitation: {round(meteo.precipIntensity * 100)}%</h2>
       <div className="section">
