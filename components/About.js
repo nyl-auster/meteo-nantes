d8049cc48c01601bea2aa16d1df05f3eebae2d28
yann <yann.boisselier@gmail.com>
2017-08-25 15:31:09 +0200

    fix typo in about section

--- components/About.js
+++ components/About.js
@@ -1,7 +1,7 @@
 const About = () => (
   <div className="section footer has-text-centered">
     <div>
-      Les données météo de cette application sont fournis par le service <a href="https://darksky.net/">Darksy</a>
+      Les données météo de cette application sont fournies par le service <a href="https://darksky.net/">Darksy</a>
     </div>
     <div>
       L'application est développée par <a href="https://yineo.fr">Yineo</a>
