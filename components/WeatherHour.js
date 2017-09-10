0c7a93d9209d58a530f755c457dbf5683badf698
yann <yann.boisselier@gmail.com>
2017-09-10 11:21:47 +0200

    suppression des 'oui', 'non', 'peut-etre'

--- components/WeatherHour.js
+++ components/WeatherHour.js
@@ -7,24 +7,20 @@ const WillItRain = ({ precipProbability }) => {
   let WillItRainText = ''
   let WillItRainClass = ''
   if (precipProbability === 0) {
-    WillItRainText = 'Non'
     WillItRainClass = 'no' 
   }
   if (precipProbability > 0 && precipProbability < 50) {
-    WillItRainText = 'Peut-être'
     WillItRainClass = 'maybe' 
   }
   if (precipProbability >= 50 && precipProbability < 90) {
-    WillItRainText = 'Sûrement'
     WillItRainClass = 'surely' 
   }
   if (precipProbability >= 90) {
-    WillItRainText = 'oui'
     WillItRainClass = 'yes' 
   }
   return (
     <div>
-      <span className={`will-it-rain ${WillItRainClass}`}>{WillItRainText}</span>
+      <span className={`will-it-rain ${WillItRainClass}`}>{round(precipProbability*100)} %</span>
       <style jsx>{`
       .will-it-rain {
         display:inline-block;
@@ -55,16 +51,15 @@ const ReadableTime = ({ unixtime }) => {
   let hour = moment.unix(unixtime).format('H')
   const nextHour = hour == '23' ? '00' : String((parseInt(hour) + 1))
   return (
-    <div> de {hour}h à {nextHour} h</div>
+    <div> <strong>de {hour}h à {nextHour} h</strong></div>
   )
 }
 
 const WeatherHour = ({ meteo }) => {
   return (
     <div className="content section">
-      <h2> <ReadableTime unixtime={meteo.time} /> </h2>
+      <h2> Probabilité de précipitation <ReadableTime unixtime={meteo.time} /> </h2>
       <WillItRain precipProbability={meteo.precipProbability} />
-      <h2>Probabilité de précipitation: {round(meteo.precipProbability * 100)}% </h2>
       <h2>Intensité de la précipitation: {round(meteo.precipIntensity * 100)}%</h2>
       <div className="section">
         <ReactAnimatedWeather
