5ed4f4fc242f8684abb8433a779dcdef38c34e7c
yann <yann.boisselier@gmail.com>
2017-09-10 12:02:06 +0200

    try to add pwa support

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,29 @@ import Header from './Header'
 import Head from 'next/head'
 import Navigation from './Navigation'
 
-const Layout = ({ children }) => (
-  <div>
-    <Head>
-      <link rel="stylesheet" type="text/css" href="/static/bulma-5.1.css" />
-    </Head>
-    <Header />
-    {children}
-  </div>
-)
+const Layout = ({ children }) => {
+  const iconPath = '/static/images/icon.svg'
+  return (
+    <div>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/static/bulma-5.1.css" />
+
+        {/*PWA*/}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="Va-t-il pleuvoir ?" />
+        <meta name="apple-mobile-web-app-title" content="Va-t-il pleuvoir ?" />
+        <meta name="msapplication-starturl" content="/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="icon" type="image/svg" href={iconPath} />
+        <link rel="apple-touch-icon" type="image/svg" href={iconPath} />
+        {/*END PWA*/}
+
+      </Head>
+      <Header />
+      {children}
+    </div>
+  )
+}
 
 export default Layout
